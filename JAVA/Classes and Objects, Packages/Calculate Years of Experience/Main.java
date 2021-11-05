e9462465badfcf89c0170c2ca3233832b45245b6
Simran Chauhan <49764874+simran-cha@users.noreply.github.com>
2021-11-05 16:21:42 +0530

    Update Main.java

--- JAVA/Classes and Objects, Packages/Calculate Years of Experience/Main.java
+++ JAVA/Classes and Objects, Packages/Calculate Years of Experience/Main.java
@@ -1,23 +1,24 @@
-import java.util.*;
-import java.text.*;
-public class Main 
-{
-public static void main(String[] args) 
-{
-Scanner sc=new Scanner(System.in);
-String join = sc.next();
-SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
-try 
-{ sdf.setLenient(false); 
-Date d1 = sdf.parse(join); 
-Date d2 = new Date(); 
-long diff = d2.getTime() - d1.getTime(); 
-long l1 = (24*60*60*1000); 
-long l = l1*365; 
-long year=diff/l; 
-System.out.println(year+" years"); 
-} 
-catch(ParseException e) {} 
-}
-    
+import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+public class Main {
+	public static void main(String[] args) {
+	    	Scanner sc=new Scanner(System.in);
+
+	    String str2="15/12/2020";
+	    String str1=sc.next();
+	    SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
+	    sdf.setLenient(false);
+	    Date d1,d2;
+	    try{
+		d1=sdf.parse(str1);
+		d2=sdf.parse(str2);
+	}catch(ParseException e) {
+		return;
+	}
+	long time=(d2.getTime() - d1.getTime())/1000/60/60/24/365;
+	System.out.println(time+" years");
+	}
 }
